b47549c6e37c14315cd8771831dd6b873b1dfb9b
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2025-06-09 09:33:51 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 82.00 ms, Memory: 205 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] numbers) {
+        int [] answer = new int[numbers.length];
+        Stack<Integer> stk = new Stack<>();
+        stk.add(0);
+        for (int i=1; i<numbers.length; i++){
+            while(!stk.isEmpty()&&(numbers[stk.peek()]<numbers[i])){
+                answer[stk.pop()] = numbers[i];
+            }
+            stk.add(i);
+        }
+        while(!stk.isEmpty()){
+            answer[stk.pop()] = -1;
+        }
+        return answer;
+    }
+}
